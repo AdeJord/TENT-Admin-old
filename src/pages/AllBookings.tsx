8c4d421a7128f2718833f99a341512daa54a1513
Adrian Jordan <adejord@gmail.com>
2023-11-07 17:49:03 +0000

    more done

--- src/pages/AllBookings.tsx
+++ src/pages/AllBookings.tsx
@@ -1,67 +1,103 @@
-import React, { useEffect, useState } from 'react';
-import { Root, Table, Container, ContainerPartition, Header } from '../styles';
-import axios from 'axios';
+import React, { useEffect, useState } from "react";
+import {
+  Root,
+  Table,
+  CalendarContainer,
+  Button,
+  ButtonContainer,
+} from "../styles";
+import axios from "axios";
 
 // Define an interface for the booking object
 interface Booking {
   id: string;
   bookingDate: string;
+  bookingMonth: string;
   bookedBy: string;
   contactNumber: string;
   contactEmail: string;
   destination: string;
   lunchArrangements: string;
   wheelchairUsers: number;
-  Smoking: string;
+  smoking: string;
 }
 
 const AllBookings = () => {
   const [data, setData] = useState<Booking[]>([]); // Initialize data as an empty array
+  const [targetMonth, setTargetMonth] = useState<string>(
+    new Date().toLocaleString("en-UK", { month: "long" })
+  );
+
+  // Function to filter bookings by month
+  const getBookingsForMonth = (month: string) => {
+    return data.filter((booking) => booking.bookingMonth === month);
+  };
 
   useEffect(() => {
-    axios.get('http://localhost:8000/bookings')
+    // Fetch all bookings from the API
+    axios
+      .get("http://192.168.0.139:8000/bookings")
       .then((response) => {
         setData(response.data);
       })
       .catch((error) => {
-        console.error('Error fetching data:', error);
+        console.error("Error fetching data:", error);
       });
   }, []);
 
+  const handlePrevMonth = () => {
+    // Implement logic to switch to the previous month here
+    // You may need to update the 'targetMonth' state and re-fetch data for the new month.
+  };
+
+  const handleNextMonth = () => {
+    // Implement logic to switch to the next month here
+    // You may need to update the 'targetMonth' state and re-fetch data for the new month.
+  };
+
   return (
     <Root>
-      <h1>All Bookings</h1>
-      <Table>
-        <thead style={{background: 'gray'}}>
-          <tr>
-            <th>Booking Date</th>
-            <th>Booked By</th>
-            <th>Contact Number</th>
-            <th>Contact Email</th>
-            <th>Destination</th>
-            <th>Lunch Arrangements</th>
-            <th>Wheelchair Users</th>
-            <th>Smoking</th>
-          </tr>
-        </thead>
-        <tbody>
-          {data.map((item, index) => (
-            <tr key={index}>
-              <td>{item.bookingDate}</td>
-              <td>{item.bookedBy}</td>
-              <td>{item.contactNumber}</td>
-              <td>{item.contactEmail}</td>
-              <td>{item.destination}</td>
-              <td>{item.lunchArrangements}</td>
-              <td>{item.wheelchairUsers}</td>
-              <td>{item.Smoking}</td>
+      <h1>{targetMonth} Bookings</h1>
+      <CalendarContainer>
+        <ButtonContainer>
+          <Button onClick={handlePrevMonth}>Prev Month</Button>
+        </ButtonContainer>
+
+        <Table>
+          <thead style={{ background: "gray" }}>
+            <tr>
+              <th>Booking Date</th>
+              <th>Booked By</th>
+              <th>Contact Number</th>
+              <th>Contact Email</th>
+              <th>Destination</th>
+              <th>Lunch Arrangements</th>
+              <th>Wheelchair Users</th>
+              <th>Smoking</th>
             </tr>
-          ))}
-        </tbody>
-      </Table>
+          </thead>
+          <tbody>
+            {getBookingsForMonth(targetMonth).map((item, index) => (
+              <tr key={index}>
+                <td>{item.bookingDate}</td>
+                <td>{item.bookedBy}</td>
+                <td>{item.contactNumber}</td>
+                <td>{item.contactEmail}</td>
+                <td>{item.destination}</td>
+                <td>{item.lunchArrangements}</td>
+                <td>{item.wheelchairUsers}</td>
+                <td>{item.smoking}</td>
+              </tr>
+            ))}
+          </tbody>
+        </Table>
+
+        <ButtonContainer>
+          <Button onClick={handleNextMonth}>Next Month</Button>
+        </ButtonContainer>
+      </CalendarContainer>
     </Root>
   );
-}
+};
 
 export default AllBookings;
-
--- src/styles.tsx
+++ src/styles.tsx
@@ -1,101 +1,130 @@
-import styled from './styled';
+import styled from "./styled";
 
 export const Root = styled.div`
-    background-color: #EAF3E7;
-    height: 90vh;
-    width: 100vw;
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    font-size: calc(10px + 2vmin);
-    color: #051101;
-    font-family: 'Roboto, Arial, Helvetica, sans-serif';
+  background-color: #eaf3e7;
+  height: 90vh;
+  width: 100vw;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  font-size: calc(10px + 2vmin);
+  color: #051101;
+  font-family: "Roboto, Arial, Helvetica, sans-serif";
+`;
+
+export const CalendarContainer = styled.div`
+  background-color: #eaf3e7;
+  height: 90vh;
+  width: 80vw;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  font-size: calc(10px + 2vmin);
+  color: #051101;
+  font-family: "Roboto, Arial, Helvetica, sans-serif";
+`;
+
+export const ButtonContainer = styled.div`
+  background-color: #eaf3e7;
+  height: 10vh;
+  width: 100vw;
+  display: flex;
+  padding: 0.5rem;
+  flex-direction: column;
+  align-items: center;
+  font-size: calc(10px + 2vmin);
+  color: #051101;
+  font-family: "Roboto, Arial, Helvetica, sans-serif";
 `;
 
 export const Header = styled.div`
-    display: flex;
-    flex-direction: row;
-    align-items: center;
-    justify-content: center;
-    background-color: #051101;
-    height: 10vh;
-    width: 100vw;
-    color: #EAF3E7;
-    font-size: 1.5rem;
-    font-family: 'Roboto, Arial, Helvetica, sans-serif';
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+  background-color: #051101;
+  height: 10vh;
+  width: 100vw;
+  color: #eaf3e7;
+  font-size: 1.5rem;
+  font-family: "Roboto, Arial, Helvetica, sans-serif";
 `;
 
 export const Container = styled.div`
-    height: 100vh;
-    width: 60vw;
-    display: flex;
-    flex-direction: row;
-    align-items: center;
-    font-family: 'Roboto, Arial, Helvetica, sans-serif';
+  height: 100vh;
+  width: 60vw;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  font-family: "Roboto, Arial, Helvetica, sans-serif";
 `;
 
 export const ContainerPartition = styled.div`
-    height: 30vh;
-    width: 40vw;
-    display: flex;
-    flex-direction: column;
-    border: 1px solid #051101;
-    align-items: center;
-    justify-content: center;
-    font-family: 'Roboto, Arial, Helvetica, sans-serif';
-    background-color: white;
-    margin-left: 2rem;
-    margin-right: 2rem;
-    
-    &:hover {
-        background-color: #EAF3E7;
-        color: green;
-    }
+  height: 30vh;
+  width: 40vw;
+  display: flex;
+  flex-direction: column;
+  border: 1px solid #051101;
+  align-items: center;
+  justify-content: center;
+  font-family: "Roboto, Arial, Helvetica, sans-serif";
+  background-color: white;
+  margin-left: 2rem;
+  margin-right: 2rem;
+
+  &:hover {
+    background-color: #eaf3e7;
+    color: green;
+  }
 `;
 
 export const Link = styled.a`
-    text-decoration: none;
-    color: #051101;
+  text-decoration: none;
+  color: #051101;
 `;
 
 export const LinkDiv = styled(Link)`
-    color: white;
-    font-size: calc(40px + 2vmin);
-    font-family: 'Roboto, Arial, Helvetica, sans-serif';
-    text-decoration: none;
-    &:hover,
-    &:focus {
-        color: green;
-    }
-    
-    &:active {
-        color: red;
-    }
+  color: white;
+  font-size: calc(40px + 2vmin);
+  font-family: "Roboto, Arial, Helvetica, sans-serif";
+  text-decoration: none;
+  &:hover,
+  &:focus {
+    color: green;
+  }
+
+  &:active {
+    color: red;
+  }
 `;
 
-export const Button = styled.button`    
-    background-color: #051101;
-    color: #EAF3E7;
-    font-size: calc(10px + 2vmin);
-    font-family: 'Roboto, Arial, Helvetica, sans-serif';
-    border: 1px solid #051101;
-    border-radius: 5px;
-    padding: 0.5rem;
-    margin: 0.5rem;
-    width: 10vw;
-    height: 5vh;
-    &:hover {
-        background-color: #EAF3E7;
-        color: #051101;
-    }
+export const Button = styled.button`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  background-color: gray;
+  color: #051101;
+  font-size: calc(10px + 1vmin);
+  font-family: "Roboto, Arial, Helvetica, sans-serif";
+  border: 1px solid #051101;
+  border-radius: 5px;
+  padding: 0.5rem;
+  margin: 0.5rem;
+  width: 10vw;
+  height: 5vh;
+  &:hover {
+    background-color: #eaf3e7;
+    color: #051101;
+  }
 `;
 
 export const Table = styled.table`
-  font-family: 'Roboto, Arial, Helvetica, sans-serif';
+  font-family: "Roboto, Arial, Helvetica, sans-serif";
   font-size: calc(4px + 1.5vmin);
   border-collapse: collapse;
 
-  th, td {
+  th,
+  td {
     border: 1px solid #051101;
     padding: 8px;
     text-align: center;
