c0b790bcb396cfd1e19ab4ef9ce311bb79e2b21a
Adrian Jordan <adejord@gmail.com>
2023-11-25 17:49:44 +0000

    better booking form

--- src/pages/CreateBooking.tsx
+++ src/pages/CreateBooking.tsx
@@ -12,148 +12,212 @@ const inputHandler = (id: string, value: string, isValid: boolean) => {
     console.log('inputHandler')
 }
 
+const submitHandler = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault()
+    console.log('submitHandler')
+}
+
 const CreateBooking = () => {
     return (
         <Root>
             <h1>
-                Create a booking
+                Booking Form
             </h1>
             <ColumnContainer>
                 <form>
-                    <ContainerRow>
-                        <Input
-                            id='firstName'
-                            element='input'
-                            type='text'
-                            label="First Name"
-                            validators={[VALIDATOR_REQUIRE()]}
-                            errorText="Please enter your first name"
-                            onInput={inputHandler}
-                            placeholder='Your first name'
-                        />
-                        <Input
-                            id='surname'
-                            element='input'
-                            type='text'
-                            label="Surname"
-                            validators={[VALIDATOR_REQUIRE(),]}
-                            errorText="Please enter your surname"
-                            onInput={inputHandler}
-                            placeholder='Your surname'
-                        />
-                    </ContainerRow>
-                    <ContainerRow>
-                        <Input
-                            id='contactNumber'
-                            element='input'
-                            type='text'
-                            label="Contact Number"
-                            validators={[VALIDATOR_REQUIRE()]}
-                            errorText="Please enter your contact number"
-                            onInput={inputHandler}
-                            placeholder='Your contact number'
-                        />
-                        <Input
-                            id='emailAddress'
-                            element='input'
-                            type='email'
-                            label="Email Address"
-                            validators={[VALIDATOR_REQUIRE(), VALIDATOR_EMAIL()]}
-                            errorText="Please enter a valid email address"
-                            onInput={inputHandler}
-                            placeholder='Your email address'
-                        />
-                    </ContainerRow>
-                    <ContainerRow>
-                        <Input
-                            style={{ width: '120px' }}
-                            id='bookingDate'
-                            element='input'
-                            type='date'
-                            label="Booking Date"
-                            validators={[VALIDATOR_REQUIRE()]}
-                            errorText="Please enter the date you would like to book for"
-                            onInput={inputHandler}
-                        />
-                        <Input
-                            style={{ width: '120px' }}
-                            id='wheelchairUsers'
-                            element='input'
-                            type='number'
-                            //NEED MAX MIN
-                            // min="0"
-                            // max="2"
-                            label="How many wheelchair users?"
-                            validators={[
-                                VALIDATOR_REQUIRE(),
-                                VALIDATOR_MIN(0),
-                                VALIDATOR_MAX(2)
-                            ]}
-                            errorText="Please enter if there are any wheelchair users. Min 0 - Max 2"
-                            onInput={inputHandler}
-                            placeholder='Wheelchairs?'
-                        />
-                        <Input
-                            style={{ width: '100px' }}
-                            id='smoking'
-                            element='input'
-                            type='text'
-                            label="Smoking?"
-                            validators={[VALIDATOR_REQUIRE()]}
-                            errorText="Please enter whether smoking will be allowed on the trip"
-                            onInput={inputHandler}
-                            placeholder='Smoking / No Smoking'
-                        />
-                    </ContainerRow>
-                    <ContainerRow>
-                        <Input
-                            id='destination'
-                            element='input'
-                            type='text'
-                            label="Destination"
-                            validators={[VALIDATOR_REQUIRE()]}
-                            errorText="Please enter where you would like to go (Coven or Autherley)"
-                            onInput={inputHandler}
-                            placeholder='Coven or Autherley'
-                        />
-                        <Input
-                            id='lunchArrangements'
-                            element='input'
-                            type='text'
-                            label="Lunch Arrangements"
-                            validators={[VALIDATOR_REQUIRE()]}
-                            errorText="Please enter your lunch arrangements"
-                            onInput={inputHandler}
-                            placeholder='What about lunch?'
-                        />
-
-                    </ContainerRow>
-                    <ContainerRow
+                    {/* <ContainerRow> */}
+                    <Input
                         style={{
+                            width: '25vw',
+                            height: '1.5vh',
+                        }}
+                        id='firstName'
+                        element='input'
+                        type='text'
+                        label="First Name"
+                        validators={[VALIDATOR_REQUIRE()]}
+                        errorText="Please enter your first name"
+                        onInput={inputHandler}
+                        placeholder='Your first name'
+                    />
+                    <Input
+                        style={{
+                            width: '25vw',
+                            height: '1.5vh',
+                        }}
+                        id='surname'
+                        element='input'
+                        type='text'
+                        label="Surname"
+                        validators={[VALIDATOR_REQUIRE(),]}
+                        errorText="Please enter your surname"
+                        onInput={inputHandler}
+                        placeholder='Your surname'
+                    />
+                    {/* </ContainerRow>
+                    <ContainerRow> */}
+                    <Input
+                        style={{
+                            width: '25vw',
+                            height: '1.5vh',
+                        }}
+                        id='contactNumber'
+                        element='input'
+                        type='text'
+                        label="Contact Number"
+                        validators={[VALIDATOR_REQUIRE()]}
+                        errorText="Please enter your contact number"
+                        onInput={inputHandler}
+                        placeholder='Your contact number'
+                    />
+                    <Input
+                        style={{
+                            width: '25vw',
+                            height: '1.5vh',
+                        }}
+                        id='emailAddress'
+                        element='input'
+                        type='email'
+                        label="Email Address"
+                        validators={[VALIDATOR_REQUIRE(), VALIDATOR_EMAIL()]}
+                        errorText="Please enter a valid email address"
+                        onInput={inputHandler}
+                        placeholder='Your email address'
+                    />
+                    {/* </ContainerRow>
+                    <ContainerRow> */}
+                    <Input
+                        style={{
+                            width: '20vw',
+                            height: '1.5vh',
+                        }}
+                        id='bookingDate'
+                        element='input'
+                        type='date'
+                        label="Booking Date"
+                        validators={[VALIDATOR_REQUIRE()]}
+                        errorText="Please enter the date you would like to book for"
+                        onInput={inputHandler}
+                    />
+                    <Input
+                        style={{
+                            width: '15vw',
+                            height: '1.5vh',
+                        }}
+                        id='wheelchairUsers'
+                        element='input'
+                        type='number'
+                        //NEED MAX MIN
+                        // min="0"
+                        // max="2"
+                        label="How many wheelchair users?"
+                        validators={[
+                            VALIDATOR_REQUIRE(),
+                            VALIDATOR_MIN(0),
+                            VALIDATOR_MAX(2)
+                        ]}
+                        errorText="Please enter number of wheelchair users. Min 0 - Max 2"
+                        onInput={inputHandler}
+                        placeholder='Wheelchairs?'
+                    />
+                    <Input
+                        style={{
+                            width: '18vw',
+                            height: '1.5vh',
+                        }}
+                        id='smoking'
+                        element='input'
+                        type='text'
+                        label="Smoking?"
+                        validators={[VALIDATOR_REQUIRE()]}
+                        errorText="Please enter whether smoking will be allowed on the trip"
+                        onInput={inputHandler}
+                        placeholder='Smoking / No Smoking'
+                    />
+                    {/* </ContainerRow> */}
+                    {/* <ContainerRow> */}
+                    <Input
+                        style={{
+                            width: '20vw',
+                            height: '1.5vh',
+                        }}
+                        id='destination'
+                        element='input'
+                        type='text'
+                        label="Destination"
+                        validators={[VALIDATOR_REQUIRE()]}
+                        errorText="Please enter where you would like to go (Coven or Autherley)"
+                        onInput={inputHandler}
+                        placeholder='Coven or Autherley'
+                    />
+                    <Input
+                        style={{
+                            width: '20vw',
+                            height: '1.5vh',
+                        }}
+                        id='lunchArrangements'
+                        element='input'
+                        type='text'
+                        label="Lunch Arrangements"
+                        validators={[VALIDATOR_REQUIRE()]}
+                        errorText="Please enter your lunch arrangements"
+                        onInput={inputHandler}
+                        placeholder='What about lunch?'
+                    />
+
+                    {/* </ContainerRow>
+                    <ContainerRow */}
+                    {/* style={{
                             display: 'flex',
                             alignContent: 'center',
                             paddingTop: '3rem',
-                        }}>
-                        <Input
-                            style={{
-                                width: '46.5vw',
-                                height: '10vh',
-                                resize: 'none',
-                            }}
-                            id='otherRequirements'
-                            element='textarea'
-                            type='text'
-                            label="Any other requirements or notes"
-                            onInput={inputHandler}
-                            placeholder='Is there anything else you would like us to know about?'
-                        />
-                    </ContainerRow>
-                    <p style={{ fontSize: '2vw'}}>
-                        I have read and agree to the terms and conditions and group leader responsibilities etc...
-                    </p>
+                        }}> */}
+                    <Input
+                        style={{
+                            width: '25vw',
+                            height: '15vh',
+                            resize: 'none',
+                        }}
+                        id='otherRequirements'
+                        element='textarea'
+                        type='text'
+                        label="Any other requirements or notes"
+                        onInput={inputHandler}
+                        placeholder='Is there anything else you would like us to know about?'
+                    />
+                    {/* </ContainerRow> */}
+
+                    <Input
+                        style={{
+                            width: 'auto',
+                            height: 'auto',
+                            resize: 'none',
+                        }}
+                        type='checkbox'
+                        id='termsAndConditions'
+                        element='input'
+                        label="I have read and agree to the terms and conditions"
+                        validators={[VALIDATOR_REQUIRE()]}
+                        errorText="Please agree to the terms and conditions"
+                        onInput={inputHandler}
+                    ></Input>
+                    <Input
+                        style={{
+                            width: 'auto',
+                            height: 'auto',
+                            resize: 'none',
+                        }}
+                        type='checkbox'
+                        id='groupLeaderResponsibilities'
+                        element='input'
+                        label="I have read and agree to the group leader responsibilities"
+                        validators={[VALIDATOR_REQUIRE()]}
+                        errorText="Please agree to the group leader responsibilities"
+                        onInput={inputHandler}
+                    ></Input>
                 </form>
+                <Button type='submit'>SUBMIT</Button>
             </ColumnContainer>
-            <Button type='submit'>SUBMIT</Button>
         </Root>
     )
 }
